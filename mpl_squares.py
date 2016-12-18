2f3b9622cfe59fe789382708553ffc4ae9b8c1e7
rayallen20 <452857163@qq.com>
2016-12-18 22:44:06 +0800

    实现了设置曲线宽度/标题/X、Y轴的标签/坐标轴上的字体大小功能

--- mpl_squares.py
+++ mpl_squares.py
@@ -2,7 +2,21 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(squares, linewidth=5)
+
+# 设置图表标题 并给坐标轴加上标签
+
+# 标题
+plt.title("Square Numbers", fontsize=24)
+# X轴标签
+plt.xlabel("Value", fontsize=24)
+# Y轴标签
+plt.ylabel("Square of Value", fontsize=24)
+
+# 设置坐标轴上的字体大小 axis='both' 即X Y两轴同时生效
+plt.tick_params(axis='both', labelsize=14)
+
+# 显示图表
 plt.show()
 
 """
