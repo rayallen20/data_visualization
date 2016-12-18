11f196e2eb5ff2840919f5a83de2c94c858107de
rayallen20 <452857163@qq.com>
2016-12-18 23:04:24 +0800

    设置了散点图的样式 详细见代码中的注释

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,7 +1,23 @@
 # data visualization -- 绘制散点图
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4)
+# s=200 设置点的尺寸大小
+plt.scatter(2, 4, s=200)
+
+# 设置图表标题 并给坐标轴加上标签
+
+# 标题
+plt.title("Square Numbers", fontsize=24)
+# X轴标签
+plt.xlabel("Value", fontsize=24)
+# Y轴标签
+plt.ylabel("Square of Value", fontsize=24)
+
+# 设置坐标轴上的字体大小
+# which参数:major 显示主刻度 minor 显示次刻度 both 主次刻度都显示
+# labelsize 设置字号
+plt.tick_params(axis='both', which='major',  labelsize=14)
+
 plt.show()
 
 """
