6cda0ff2ae0b84a866f0b870ea782a8f6a79626e
rayallen20 <452857163@qq.com>
2016-12-18 23:09:01 +0800

    使用scatter()绘制多个点

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,14 @@
 # data visualization -- 绘制散点图
 import matplotlib.pyplot as plt
 
+# 每个点的X坐标值
+x_values = [1, 2, 3, 4, 5]
+
+# 每个点的Y坐标值
+y_values = [1, 4, 9, 16, 25]
+
 # s=200 设置点的尺寸大小
-plt.scatter(2, 4, s=200)
+plt.scatter(x_values, y_values, s=200)
 
 # 设置图表标题 并给坐标轴加上标签
 
@@ -22,4 +28,8 @@ plt.show()
 
 """
 scatter(X值,Y值) 在坐标系中绘制一个点
+"""
+
+"""
+scatter(所有点的X坐标值list, 所有点的Y坐标值list, s=num) 在坐标系中绘制多个点
 """
