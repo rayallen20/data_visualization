184f21acd79742619c4bb7767b1aa729abd19f0b
rayallen20 <452857163@qq.com>
2016-12-18 22:51:46 +0800

    修复了从0,Y开始描绘曲线的问题

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,12 @@
 # data visualization
 import matplotlib.pyplot as plt
 
+# 输入值
+input_values = [1, 2, 3, 4, 5]
+
+# 输出值
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares, linewidth=5)
+plt.plot(input_values, squares, linewidth=5)
 
 # 设置图表标题 并给坐标轴加上标签
 
@@ -31,4 +35,10 @@ squares = [1, 4, 9, 16, 25]
 (0,1) (1,4) (2,9) (3,16) (4,25)
 而我们想要的是
 (1,1) (2,4) (3,9) (4,16) (5,25)
+"""
+
+"""
+确切地讲: input_values为输入值 values为输出值 即:我输入了1 希望得到1 输入2 希望得到4 以此类推
+而不需要程序自己假设 程序的假设就是按照输出值list的key值排序 即:我输入0 希望得到1 我输入1 希望得到4
+即:每个点的X值 为输入值 Y值 为输出值
 """
