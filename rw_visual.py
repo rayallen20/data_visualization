a15c8fe6df1ff4e00b16ddb2160cee61713ffcfa
rayallen20 <452857163@qq.com>
2016-12-19 16:41:19 +0800

    实现了隐藏坐标轴功能

--- rw_visual.py
+++ rw_visual.py
@@ -22,6 +22,10 @@ while True:
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
+    # 由于我们希望看到的是点的随机漫步路径 而不是坐标轴 所以设置隐藏坐标轴
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input('Make another walk? (y/n): ')
