2ce3aa7b94c9aca870968e2f9a1c25ae7bf14b05
rayallen20 <452857163@qq.com>
2016-12-19 16:27:09 +0800

    实现了颜色映射功能

--- rw_visual.py
+++ rw_visual.py
@@ -8,7 +8,15 @@ while True:
     # 创建一个RandomWalk()实例 并将其包含的点都绘制出来
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    # 颜色映射 start
+    point_numbers = list(range(rw.num_points))
+    # 颜色映射 end
+
+    # 其中 c=point_numbers的意思 是指 前边正好有5000个点 所以每个点分配一个颜色的深度
+    # c 和 cmap两个参数同时出现时 c为一个list 其中 值较小的 颜色较浅 值较大的 颜色较深
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+                edgecolors='none', s=15)
     plt.show()
 
     keep_running = input('Make another walk? (y/n): ')
