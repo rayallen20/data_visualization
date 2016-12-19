657f9d08f36ed03d7ff59de07798d6cffa99d2e3
rayallen20 <452857163@qq.com>
2016-12-19 16:42:25 +0800

    设置了50000个点的随机漫步 并调小了每个点的大小

--- rw_visual.py
+++ rw_visual.py
@@ -6,7 +6,7 @@ from random_walk import RandomWalk
 while True:
 
     # 创建一个RandomWalk()实例 并将其包含的点都绘制出来
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     # 颜色映射 start
@@ -16,7 +16,7 @@ while True:
     # 其中 c=point_numbers的意思 是指 前边正好有5000个点 所以每个点分配一个颜色的深度
     # c 和 cmap两个参数同时出现时 c为一个list 其中 值较小的 颜色较浅 值较大的 颜色较深
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-                edgecolors='none', s=15)
+                edgecolors='none', s=1)
 
     # 着重绘制起点和终点
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
