93642780c341bfff81ba611384734fcd5301fafc
rayallen20 <452857163@qq.com>
2016-12-19 16:17:38 +0800

    实现了模拟多次随机漫步 直到用户选择不再模拟为止

--- rw_visual.py
+++ rw_visual.py
@@ -2,8 +2,15 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# 创建一个RandomWalk()实例 并将其包含的点都绘制出来
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+# 只要用户选择继续 就重新生成5000个点
+while True:
+
+    # 创建一个RandomWalk()实例 并将其包含的点都绘制出来
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input('Make another walk? (y/n): ')
+    if keep_running == 'n':
+        break
